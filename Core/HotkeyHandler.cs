408cb7b02429b3209482631bed7473742ef7f46c
krznck <krznck@proton.me>
2024-07-08 14:42:11 +0200

    Prevent double-copy on mouse copies in MainWindow

--- Core/HotkeyHandler.cs
+++ Core/HotkeyHandler.cs
@@ -75,6 +75,23 @@ namespace copy_flyouts.Core
             }
 
             ShowNewFlyout();
+
+            // we remove and return the clipboard change listener on a short timer to prevent the bug of a copy being shown twice on the main window.
+            // this is ultimately a workaround, but should not significantly mess anything up (hopefully)
+            if (affectedWindow.IsMouseOver) // note that we only need to do this when we're working with the main window, since that's when the bug happens
+            {
+                //Debug.WriteLine("Prevented double-mouse-copy bug in MainWindow.");
+                sharpClipboard.ClipboardChanged -= SharpClipboard_ClipboardChanged;
+                var timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromMilliseconds(1);
+                timer.Start();
+                timer.Tick += (sender, args) =>
+                {
+                    timer.Stop();
+                    sharpClipboard.ClipboardChanged += SharpClipboard_ClipboardChanged;
+                };
+            }
+
         }
 
         private void UserSettings_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
